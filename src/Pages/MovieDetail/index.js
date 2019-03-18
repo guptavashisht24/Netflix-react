6b62c1f55860f9b9e404a11e097daf402808942f
VijayRanghar <vijayranghar@gmail.com>
2019-03-19 00:50:33 +0530

    Add styling movie detail page

--- src/Pages/MovieDetail/index.js
+++ src/Pages/MovieDetail/index.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import { getMovieDetail } from '../../Actions'
 import Header from '../../Components/header/header.js'
-import  * as styles from './style'
+import  { Grid, H2, Ul, Li } from './style'
 
 class MovieDetail extends Component {
   componentDidMount() {
@@ -14,29 +14,43 @@ class MovieDetail extends Component {
   render () {
     //console.log(this.props.movie)
     //return (<div></div>)
-    const { original_title, overview, genres, vote_average, release_date, runtime } = this.props.movie
+    const { adult, poster_path, original_title, overview, genres, vote_average, release_date, runtime } = this.props.movie
     const renderGenres = genres && genres.map((item, index) => (
       <li key={index}>{item.name}</li>
     ))
+    const category = adult ? 'A' : 'U/A'
+    const renderReleaseData = release_date && release_date.slice(0,4)
+    const hour = Math.floor(runtime/60)
+    const minutes = runtime%60
+    const backgroundStyle = {
+      backgroundImage: `url(http://image.tmdb.org/t/p/original${poster_path})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '400px',
+    }
     return (
       <div>
         <Header />
-        <Container>
+        <Grid>
           <Row>
-            <Col md={5}>
-              a
-              <Hello>asdas</Hello>
+            <Col md={5} style={backgroundStyle}>
             </Col>
             <Col sm={7}>
-              <div>{original_title}</div>
-              <ul>{renderGenres}</ul>
-            <div>{vote_average}</div>
-              <div>{release_date}</div>
-              <div>{runtime} minutes</div>
+              <H2>{original_title}</H2>
+              <Ul>
+                <Li>{renderReleaseData}</Li>
+                <Li>{hour}h {minutes}min</Li>
+                <Li>{category}</Li>
+              </Ul>
+              <Ul>
+                {renderGenres}
+              </Ul>  
+              <div>{vote_average}</div>
               <div>{overview}</div>
             </Col>
           </Row>
-        </Container>
+        </Grid>
       </div>
     )
   }
--- src/Pages/MovieDetail/style.js
+++ src/Pages/MovieDetail/style.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
+import { Container } from 'react-bootstrap'
 
-export const Hello = styled.h1`
-  font-size: 35px;
+export const Grid = styled(Container)`
+  margin-top: 125px;
+`
+export const H2 = styled.h2`
+  font-size: 24px;
+  font-weight: 300;
+  line-height: 1.4;
+  margin-bottom: 1em;
+`
+export const Ul = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+`
+export const Li = styled.li`
+  color: #9BA09A;
+  padding: 0 10px;
+  line-height: 1;
+  font-size: 13px;
+  border-right: 1px solid #9BA09A;
+  :first-child {
+    padding-left: 0;
+  }
+  :last-child {
+    border-right: none;
+  }
 `
